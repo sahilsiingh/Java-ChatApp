73eb28f0577919d4cb068e5d8cfd632f16210b63
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-11-13 12:46:52 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 import java.io.BufferedReader;   
 import java.io.InputStreamReader; 
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.Socket; 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
